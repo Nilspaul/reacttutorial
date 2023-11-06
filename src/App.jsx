79d2b5a50aa0e7e5fbd271586407cac9573f8c6c
Nilspaul <nilspaul27@gmail.com>
2023-11-06 17:14:58 +0100

    data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from '/assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Hier führen wir die Anfrage an deine Express API durch
+    fetch('http://127.0.0.1:3317/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error('Fehler bei der Anfrage:', error));
+  }, []);
 
   return (
     <>
@@ -24,12 +33,27 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <div>
+          <h2>Produkte:</h2>
+          <ul>
+            {products.map((product, index) => (
+              <li key={index}>
+                <div>
+                  <h3>{product.title}</h3>
+                  <p>{product.desc}</p>
+                  <p>{product.image}</p>
+                  <img src={product.image} alt={product.title} />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
