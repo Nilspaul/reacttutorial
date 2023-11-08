6f879a31f4b221cb21d23eea21cc65111debda57
Nilspaul <nilspaul27@gmail.com>
2023-11-08 11:36:33 +0100

    small contactform - no functionality

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,30 @@
-
+import "../css/contactForm.css"
 
 const ContactForm = () => {
   return (
     <div>
       <h2>Kontaktiere Uns</h2>
-    </div>
+      <form action="action_page.php">
+
+        <label>First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+
+        <label >Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+
+        <label>Country</label>
+        <select id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+
+        <label>Subject</label>
+        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+
+        <input type="submit" value="Submit"/>
+      </form>
+    </div> 
   );
 };
 
