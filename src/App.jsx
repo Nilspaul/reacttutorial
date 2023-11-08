5b6d93300c480c42996d8939ffd2de854c7ab907
Nilspaul <nilspaul27@gmail.com>
2023-11-08 09:39:36 +0100

    productList pice added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react';
-import reactLogo from '/assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Productlist from './components/Productlist'; // Importiere die Productlist-Komponente
 
 function App() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-
+  const handleButtonClick = () => {
+    console.log(count)
+  };
   useEffect(() => {
     // Hier führen wir die Anfrage an deine Express API durch
     fetch('http://127.0.0.1:3317/products')
@@ -18,23 +18,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React - Tutorial</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <Productlist list={products} /> {/* Verwende die Productlist-Komponente hier */}
+        <button onClick={handleButtonClick}>
+          count is {count}
+        </button>
+        <Productlist list={products} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/components/Productlist.jsx
+++ src/components/Productlist.jsx
@@ -1,21 +1,41 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Productlist = (props) => (
-  <div>
-    <h2>Produkte:</h2>
-    <div className="product-list"> {/* Verwende eine äußere Container-Div mit der Klasse product-list */}
-      {props.list.map((product, index) => (
-        <div key={index} className="product"> {/* Verwende eine Div für jedes Produkt mit der Klasse product */}
-          <div>
-            <h3>{product.title}</h3>
-            <p>{product.desc}</p>
-            <img src={product.image} alt={product.title} />
+const Productlist = (props) => {
+  const [modifiedList, setModifiedList] = useState([]);
+
+  useEffect(() => {
+    const modifiedProducts = props.list.map((product) => ({
+      ...product,
+      price: calculatePrice(product), //Preisberechnungsfunktion aufrufen
+    }));
+
+    // Aktualisiere den Zustand mit der bearbeiteten Liste
+    setModifiedList(modifiedProducts);
+  }, [props.list]); // Führe den Effekt aus, wenn props.list sich ändert
+
+  const calculatePrice = (product) => {
+    return product.title.length * 10;
+  };
+
+  return (
+    <div>
+      <h2>Produkte:</h2>
+      <div className="product-list">
+        {modifiedList.map((product, index) => (
+          <div key={index} className="product">
+            <div>
+              <h3>{product.title}</h3>
+              <p>{product.desc}</p>
+              <img src={product.image} alt={product.title} />
+              <p>Preis: {product.price} EUR</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Productlist.propTypes = {
   list: PropTypes.array
