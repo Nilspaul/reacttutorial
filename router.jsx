58e5f5c335786825009f79cd54105e8d46268aad
Leander Theiß <apps@leander.lt>
2023-11-08 13:34:29 +0100

    Update: Statische Daten auslagern

--- router.jsx
+++ router.jsx
@@ -4,32 +4,13 @@ import { Route, Routes } from 'react-router-dom';
 import Productlist from './src/components/Productlist';
 import Main from './src/components/Mainpage';
 import ContactForm from './src/components/ContactForm';
+import staticProducts from './src/data/staticProducts';
 
 const AppRouter = () => {
-  const products = [
-    {
-      title: "Burger",
-      desc: "LULW",
-      image: "/assets/burger.png"
-    },
-    {
-      title: "Hotdog",
-      desc: "LULW",
-      image: "/assets/hotdog.png"
-    },
-    {
-      title: "Bowl",
-      desc: "LULW",
-      image: "/assets/bowl.png"
-    }
-  ];
-
-
-
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/produkte" element={<Productlist title="test der Produkte" list={products} />} />
+      <Route path="/produkte" element={<Productlist title="test der Produkte" list={staticProducts} />} />
       <Route path="/kontakt" element={<ContactForm/>} />
     </Routes>
   );
---
+++ src/data/staticProducts.jsx
@@ -0,0 +1,19 @@
+const staticProducts = [
+  {
+    title: "Burger",
+    desc: "Lecker Burger",
+    image: "/assets/burger.png"
+  },
+  {
+    title: "Hotdog",
+    desc: "Nen Hot ohne Dog",
+    image: "/assets/hotdog.png"
+  },
+  {
+    title: "Bowl",
+    desc: "Pflanzlich und so",
+    image: "/assets/bowl.png"
+  }
+];
+
+export default staticProducts
